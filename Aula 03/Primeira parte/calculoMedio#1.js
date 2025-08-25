faeedfbe08c39bbda9d1171f61d66c3e216aa2a1
Bruna <bantonellodj@gmail.com>
2025-08-25 11:54:12 +0100

    Aula 3

---
+++ Aula 03/Primeira parte/calculoMedio#1.js
@@ -0,0 +1,5 @@
+function valorMedio(valorUm, valorDois, valorTres, qntDeValoresInformados){
+  return (valorUm + valorDois + valorTres) / qntDeValoresInformados
+}
+const resultadoFinal = valorMedio(5.00, 10.00, 25.00, 3)
+console.log(resultadoFinal)
---
+++ Aula 03/Primeira parte/calculoMedio#3.js
@@ -0,0 +1,6 @@
+function somar(primeiroNumero, segundoNumero, terceiroNumero){
+    return primeiroNumero + segundoNumero + terceiroNumero 
+}
+
+const resultadoDaMedia = somar(65, 75, 90) /3
+console.log(resultadoDaMedia)
